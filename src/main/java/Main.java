b16d471b4523747ce26334d813281669dab808e8
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2022-10-24 17:56:35 +0200

    old Files

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import ca.uhn.hl7v2.HL7Exception;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws HL7Exception, IOException {
+        Script script = new Script();
+        script.ausgabe();
+
+    }
+}
