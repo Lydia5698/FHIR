23a1a6d7cde8ef363c610676bdd72199a5b62904
Joshua Wiedekopf <joshua.wiedekopf@uksh.de>
2022-12-05 14:34:25 +0100

    tweaks for running with very many files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import ca.uhn.hl7v2.HL7Exception;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws HL7Exception, IOException {
-        Script script = new Script();
+        if (args.length != 2) {
+            System.err.println("Please provide an argument with the input folder, and an argument for the output file");
+            System.exit(1);
+        }
+        String inputFolder = args[0];
+        String outputFilename = args[1];
+        System.out.printf("Reading files from %s, writing to %s\n", inputFolder, outputFilename);
+        Script script = new Script(inputFolder, outputFilename);
         script.ausgabe();
 
     }
--- src/main/java/Script.java
+++ src/main/java/Script.java
@@ -10,44 +10,71 @@ import ca.uhn.hl7v2.util.Hl7InputStreamMessageIterator;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 
 public class Script {
+    private final String startFolder;
+    private final String outputFilename;
     HapiContext context = new DefaultHapiContext();
     Parser p = context.getPipeParser();
 
-    public Script() {
+    final int maximumRepetition = 100;
 
+    public Script(String startFolder, String outputFilename) {
+        this.startFolder = startFolder;
+        this.outputFilename = outputFilename;
     }
 
     public void ausgabe() throws HL7Exception, IOException {
-        File folder = new File("src/main/resources/data/");
-        File[] listOfFiles = folder.listFiles((dir, name) -> !name.equals(".DS_Store") && !name.equals(".gitkeep"));
-        assert listOfFiles != null;
-        for (File file : listOfFiles){
-            InputStream inputStream = new FileInputStream(file.getAbsolutePath());
-            Hl7InputStreamMessageIterator streamMessageIterator = new Hl7InputStreamMessageIterator(inputStream);
-            Message message = streamMessageIterator.next();
-            ORU_R01 oruR01 = (ORU_R01) p.parse(message.encode());
-            ORU_R01_ORDER_OBSERVATION orderObservation;
-            // When there are more than one OBX,OBR,ORC
-            for (int j = 0; j<15; j++){
-                if(!oruR01.getPATIENT_RESULT().getORDER_OBSERVATION(j).isEmpty()){
-                    orderObservation = oruR01.getPATIENT_RESULT().getORDER_OBSERVATION(j);
-                    OBX obx = orderObservation.getOBSERVATION().getOBX();
-                    String identifier = "";
-                    for (int n = 0; n < obx.getObservationValue().length; n++){
-                        identifier = identifier.concat(obx.getObservationValue(n).encode());
+
+        //Stream<Path> streamOfPaths;
+        Path outputPath = Path.of(this.outputFilename);
+        HashSet<String> contentList;
+        if (outputPath.toFile().exists()) {
+            contentList = new HashSet<>(Files.readAllLines(outputPath, StandardCharsets.ISO_8859_1));
+        } else {
+            contentList = new HashSet<>();
+        }
+        try (Writer writer = new BufferedWriter(new FileWriter(outputPath.toFile(), StandardCharsets.ISO_8859_1, true))) {
+            System.out.printf("Collecting files from input directory %s\n", this.startFolder);
+
+            try (Stream<Path> streamOfPaths = Files.walk(Path.of(this.startFolder)).filter(p ->
+                    p.toFile().isFile() && !p.endsWith(".DS_Store") && !p.endsWith(".gitkeep"))) {
+
+                Iterator<Path> pathIterator = streamOfPaths.iterator();
+                long pathIndex = 0;
+                while (pathIterator.hasNext()) {
+                    Path path = pathIterator.next();
+                    File file = path.toFile();
+                    pathIndex++;
+                    if (pathIndex % 100 == 0) {
+                        System.out.printf("Reading file %d - %s\n", pathIndex, path);
                     }
-                    Path path = Path.of("src/main/resources/observationValue.txt");
-                    HashSet<String> contentList = new HashSet<>(Files.readAllLines(path, StandardCharsets.UTF_8));
-                    Writer writer = new BufferedWriter(new FileWriter("src/main/resources/observationValue.txt", true));
-                    if (!contentList.contains(identifier)) {
-                        writer.write(identifier);
-                        writer.write("\n");
-                        writer.close();
+
+                    InputStream inputStream = new FileInputStream(file.getAbsolutePath());
+                    Hl7InputStreamMessageIterator streamMessageIterator = new Hl7InputStreamMessageIterator(inputStream);
+                    Message message = streamMessageIterator.next();
+                    ORU_R01 oruR01 = (ORU_R01) p.parse(message.encode());
+                    ORU_R01_ORDER_OBSERVATION orderObservation;
+                    // When there are more than one OBX,OBR,ORC
+                    for (int j = 0; j < maximumRepetition; j++) {
+                        if (!oruR01.getPATIENT_RESULT().getORDER_OBSERVATION(j).isEmpty()) {
+                            orderObservation = oruR01.getPATIENT_RESULT().getORDER_OBSERVATION(j);
+                            OBX obx = orderObservation.getOBSERVATION().getOBX();
+                            String identifier = "";
+                            for (int n = 0; n < obx.getObservationValue().length; n++) {
+                                identifier = identifier.concat(obx.getObservationValue(n).encode());
+                            }
+                            if (!contentList.contains(identifier)) {
+                                writer.write(identifier);
+                                writer.write("\n");
+                            }
+                        }
                     }
                 }
             }
